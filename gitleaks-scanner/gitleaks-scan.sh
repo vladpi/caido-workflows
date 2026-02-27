08798ed57db16d5c2ac16bfa7acf79b6f0a026e7
Vlad Pastushenko <iam@vladpi.me>
2026-02-27 16:40:18 +0300

    fix: minor shell fixes

--- gitleaks-scanner/gitleaks-scan.sh
+++ gitleaks-scanner/gitleaks-scan.sh
@@ -1,13 +1,1 @@
-RESPONSE_BODY=$(cat - | jq -r .response | base64 -d | sed '1,/^\r$/d')
-
-if [ -z "$RESPONSE_BODY" ]; then
-  echo '[]'
-  exit 0
-fi
-
-echo "$RESPONSE_BODY" | gitleaks stdin --report-format json -r - --no-banner 2>/dev/null
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -ne 1 ]; then
-  echo '[]'
-fi
+cat - | jq -r .response | base64 -d | sed '1,/^\r$/d' | gitleaks stdin --report-format json -r - --no-banner --no-color --exit-code 0
--- jsluice-urls/jsluice-extract.sh
+++ jsluice-urls/jsluice-extract.sh
@@ -1,8 +1,1 @@
-RESPONSE_BODY=$(cat - | jq -r .response | base64 -d | sed '1,/^\r$/d')
-
-if [ -z "$RESPONSE_BODY" ]; then
-  echo ''
-  exit 0
-fi
-
-echo "$RESPONSE_BODY" | jsluice urls -j 2>/dev/null
+cat - | jq -r .response | base64 -d | sed '1,/^\r$/d' | jsluice urls -j
